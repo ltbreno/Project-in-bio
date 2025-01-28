e2ae783756e01f12a41209f9528638aac664834f
Breno Lopes <brenosas2003@gmail.com>
2025-01-28 09:53:38 -0300

    create hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div>helloword</div>
+    <div className="max-w-7xl mx-auto">
+      <Hero />
+      {/*<Header />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
+    </div>
   );
 }
